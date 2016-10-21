20033675e577d9e8ba515b48e1cee87807a11dd6
piotrek <queader.w@gmail.com>
2016-10-21 12:39:59 +0200

    upp

---
+++ DIffReportGenerator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIffReportGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Usage: <file1 - older> <file2 - later>");
+
+
+        }
+    }
+}
